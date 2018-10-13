8b7697b6e1c932ddbee3bda5f7c2556b1274946e
MutthuBhagya <muttub34@gmail.com>
2018-10-13 12:17:19 +0000

    polling changed to 30M

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		//Prints "Hello, Muttugouda" to the terminal windows.
-		System.out.println("Hello, Muttugouda and Bhagyashree");
+		System.out.println("Hello, Muttugouda and Bhagyashree and polling time has been changed to every 30M");
 	}
 }
